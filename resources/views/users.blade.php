c395d5e5ab62d1efac13b1edd0de3e244c662668
Ali Yousef <Ali Yousef@DESKTOP-LNMBCJ1>
2023-04-27 15:41:32 +0200

    first commit

---
+++ resources/views/users.blade.php
@@ -0,0 +1,6 @@
+<div class="container" style="text-align: center ;background-color:rgb(82, 86, 89) ; color: white">
+<h1>power bi</h1>
+
+<h4>تقرير الاصناف</h4>
+<iframe title="الاصناف - Page 1" width="820" height="740" src="https://app.powerbi.com/reportEmbed?reportId=b84ce7b2-a9a4-43fc-b9de-e099a78a47c3&autoAuth=true&ctid=38e3afb5-977e-457a-8f28-6fdbb56c2789" frameborder="0" allowFullScreen="true"></iframe>
+</div>
